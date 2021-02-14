d8c3ae0d624261667a175b4a404ea522b4d4a933
heer77331 <47327787+heer77331@users.noreply.github.com>
2021-02-14 16:10:16 +0530

    Delete MLAPLI directory

--- MLAPLI/Api/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from . models import approvals
-# Register your models here.
-admin.site.register(approvals)
--- MLAPLI/Api/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from . models import approvals
-
-class MyForm(ModelForm):
-	class Meta:
-		model=approvals
-		fields = '__all__'
-		#exclude = 'firstname'
